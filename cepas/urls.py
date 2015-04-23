35eafb83f59a61c99a6b96673dff6304dc272aa2
Rafael Mercedes <rafaelmersant@gmail.com>
2015-04-23 18:55:32 -0400

    Update urls.py

--- cepas/urls.py
+++ cepas/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import patterns, include, url
-
+rom django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,4 +13,8 @@ urlpatterns = patterns('',
     url(r'^$', 'cepas.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'accounts/login/$', 'cepas.views.login', name='login')
+)
+
+urlpatterns += patterns('',
+    url(r'static/(?P<path>.*)/$', 'django.views.static.serve', {'document_root:': settings.STATIC_ROOT,}),
 )
