1276207752d000a6799a1a9c4729251272609cc2
Rafael Mercedes <rafaelmersant@gmail.com>
2015-06-14 14:20:08 -0400

    Registros Creados y registros modificados ordenados por cantidadTotal.

--- administracion/views.py
+++ administracion/views.py
@@ -80,7 +80,7 @@ class GrupoDigitadores(LoginRequiredMixin, DetailView):
 										LEFT OUTER JOIN administracion_iglesia i on i.id = m.iglesia_id \
 										GROUP BY iglesia_id, modificadoPor_id \
 										HAVING cantidadTotal > 1 and username <> \'cepas\' \
-										ORDER BY modificado desc')
+										ORDER BY cantidadTotal desc')
 
 		for registro in registros:
 			data.append({
@@ -109,7 +109,7 @@ class GrupoDigitadoresCreados(LoginRequiredMixin, DetailView):
 										LEFT OUTER JOIN administracion_iglesia i on i.id = m.iglesia_id \
 										GROUP BY iglesia_id, creadoPor_id \
 										HAVING cantidadTotal > 1 and username <> \'cepas\' \
-										ORDER BY creadoFecha desc')
+										ORDER BY cantidadTotal desc')
 
 		for registro in registros:
 			data.append({
